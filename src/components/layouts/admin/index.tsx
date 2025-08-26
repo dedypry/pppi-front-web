bae3d001d1af86948fbeddbe1c749d2f3c447718
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-08-26 14:10:59 +0700

    update

--- src/components/layouts/admin/index.tsx
+++ src/components/layouts/admin/index.tsx
@@ -1,24 +1,45 @@
-import { useState, useEffect, ReactNode } from "react";
-import { useMediaQuery } from "react-responsive";
+import { useState, ReactNode, useEffect } from "react";
 import { Drawer, DrawerContent } from "@heroui/react";
 import { Outlet } from "react-router-dom";
 
 import SidebarMenu from "./partials/sidebar-menu";
 import NavbarComponent from "./partials/navbar";
 
-import { responsive } from "@/config/responsive";
 import AuthGuard from "@/guards/AuthGuard";
 
 interface Props {
   children?: ReactNode;
 }
+
 export default function AdminLayout({ children }: Props) {
   const [isOpen, setIsOpen] = useState(true);
-  const isMobile = useMediaQuery(responsive.mobile);
+
+  const isMobile = useMediaQuery("(max-width: 1023px)");
+
+  console.log(isMobile);
 
   useEffect(() => {
     setIsOpen(false);
   }, [isMobile]);
+
+  function useMediaQuery(query: string) {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+      const media = window.matchMedia(query);
+
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+      const listener = () => setMatches(media.matches);
+
+      media.addEventListener("change", listener);
+
+      return () => media.removeEventListener("change", listener);
+    }, [matches, query]);
+
+    return matches;
+  }
 
   return (
     <AuthGuard>
@@ -34,7 +55,7 @@ export default function AdminLayout({ children }: Props) {
         </Drawer>
       ) : (
         <aside
-          className={`translation-all fixed h-screen w-[266px] transform bg-gradient-to-tr from-primary-900 to-primary-600 pl-3 shadow-lg shadow-primary-200 duration-300 ease-in-out ${
+          className={`z-50 translation-all fixed h-screen w-[266px] transform bg-gradient-to-tr from-primary-900 to-primary-600 pl-3 shadow-lg shadow-primary-200 duration-300 ease-in-out ${
             !isOpen ? "-translate-x-full" : "translate-x-0"
           } ${isMobile && "-translate-x-full"} `}
         >
